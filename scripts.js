54f6b40332ac32dae09929e2df2d1e08663299ce
Vanesa Edo <vanesa.en@gmail.com>
2024-02-20 21:40:26 +0100

    1st in progress.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+
+let parent = document.querySelector('a');
+let child = document.createAttribute('src','#');
+
+child.appendChild();
+parent.appendChild(child);
+
+const child = document.createElement("li");
+const tarea = document.createTextNode(array[i]); 
+child.appendChild(tarea);
+parent.appendChild(child);
